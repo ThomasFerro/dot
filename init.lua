853dc2dd31971bef88b0321484a2c176ae100061
Thomas Ferro <thomas.ferro@agicap.com>
2023-11-14 18:51:12 +0100

    nvim config - first version

--- init.lua
+++ init.lua
@@ -15,7 +15,6 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-  -- TODO: Lire la doc 
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
 
@@ -81,13 +80,14 @@ require('lazy').setup({
     },
   },
 
-  -- TODO: Trouver un autre thème
   {
-    'navarasu/onedark.nvim',
+    'bluz71/vim-moonfly-colors',
+    name = "moonfly",
+    lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
-    end,
+      vim.cmd [[colorscheme moonfly]]
+    end
   },
 
   {
@@ -182,25 +182,18 @@ require('telescope').setup {
   },
 }
 
--- Enable telescope fzf native, if installed
 pcall(require('telescope').load_extension, 'fzf')
 
--- Telescope live_grep in git root
--- Function to find the git root directory based on the current buffer's path
 local function find_git_root()
-  -- Use the current buffer's path as the starting point for the git search
   local current_file = vim.api.nvim_buf_get_name(0)
   local current_dir
   local cwd = vim.fn.getcwd()
-  -- If the buffer is not associated with a file, return nil
   if current_file == "" then
     current_dir = cwd
   else
-    -- Extract the directory from the current file's path
     current_dir = vim.fn.fnamemodify(current_file, ":h")
   end
 
-  -- Find the Git root directory from the current file's path
   local git_root = vim.fn.systemlist("git -C " .. vim.fn.escape(current_dir, " ") .. " rev-parse --show-toplevel")[1]
   if vim.v.shell_error ~= 0 then
     print("Not a git repository. Searching on current working directory")
